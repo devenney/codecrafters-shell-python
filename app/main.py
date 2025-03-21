072be25a3941b8c2672d486c05bdea9e5dd5105e
Brendan Devenney <brendan@devenney.io>
2025-03-21 16:16:47 +0000

    refactor: match statement simplified
    Signed-off-by: Brendan Devenney <brendan@devenney.io>

--- app/main.py
+++ app/main.py
@@ -3,23 +3,22 @@ import sys
 
 def main():
     while True:
+        # Print prompt
         sys.stdout.write("$ ")
 
-        # Wait for user input
-        user_input = input()
-        command = user_input.split()[0]
-        arg_str = user_input.removeprefix(command + ' ')
+        # Collect input
+        cmd = input().split(" ")
 
-        match command:
-            case "echo":
-                print(f"{arg_str}")
-            case "exit":
-                if len(arg_str) > 0:
-                    exit(int(arg_str))
-                else:
-                    exit()
+        # Process input
+        match cmd:
+            case ["echo", *words]:
+                print(f"{' '.join(words)}")
+            case ["exit", code]:
+                exit(int(code))
+            case [""]:
+                continue
             case _:
-                print(f"{command}: command not found")
+                print(f"{cmd[0]}: command not found")
 
 
 if __name__ == "__main__":
